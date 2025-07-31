321ea8ed5ea00d80ab6090512d3784848babbd7e
what-the-sherlock <aashu0333@gmail.com>
2025-07-31 23:09:22 +0530

    added theme toggle in navbar using shadcn

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import Theme from "./Theme";
+
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full p-6 dark:shadow-none sm:px-12 gap-5">
@@ -20,7 +22,7 @@ const Navbar = () => {
       <p>Global Search</p>
 
       <div className="flex-between gap-5">
-        Themes
+        <Theme/>
       </div>
     </nav>
   );
