7c9116ae64a7d74692aa96f339041b74c5a7a8e8
what-the-sherlock <aashu0333@gmail.com>
2025-07-31 22:55:24 +0530

    added logo properties in navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 
+import Navbar from "@/components/navigation/navbar";
+
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
   variable: "--font-inter",
@@ -39,6 +41,7 @@ export default function RootLayout({
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange>
+            <Navbar/>
           {children}
         </ThemeProvider>
       </body>
---
+++ components/navigation/navbar/index.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full p-6 dark:shadow-none sm:px-12 gap-5">
+      <Link href="/" className="flex items-center gap-1">
+        <Image 
+            src="/images/site-logo.svg"
+            width={23}
+            height={23}
+            alt="Devflow logo"
+        />
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+            Chai<span className="text-primary-500">&</span>Code<span/>
+        </p>
+      </Link>
+
+      <p>Global Search</p>
+
+      <div className="flex-between gap-5">
+        Themes
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
